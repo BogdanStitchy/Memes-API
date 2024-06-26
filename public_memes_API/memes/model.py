0b4660acb55aa63255a48f4efc2df724294abc35
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-06-26 22:03:33 +0300

    fixed Base

--- public_memes_API/memes/model.py
+++ public_memes_API/memes/model.py
@@ -1,7 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from public_memes_API.db import Base
 
 
 class Meme(Base):
