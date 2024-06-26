64bb3b1a7f6e60b6c5196cfcb19f1c98a7e77a9a
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-06-26 12:52:32 +0300

    Corrected format returned file to extension of source file. Added methods to get list all files in storage and to delete file

--- private_media_service/memes/router.py
+++ private_media_service/memes/router.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, HTTPException, UploadFile, File
+from fastapi import APIRouter, HTTPException, UploadFile, File, Response, status
 from starlette.responses import StreamingResponse
+import mimetypes
+
 from private_media_service.memes.config_s3 import s3_client
 
 router = APIRouter(
@@ -12,15 +14,39 @@ router = APIRouter(
 async def upload_file(file: UploadFile = File(...)):
     try:
         s3_client.upload_fileobj(file.file, "memes", file.filename)
-        return {"filename": file.filename}
+        return Response(status_code=status.HTTP_201_CREATED)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/download/{filename}")
-async def download_file(filename: str):
+async def download_file(filename: str) -> StreamingResponse:
+    def get_mime_type(filename: str):
+        mime_type, _ = mimetypes.guess_type(filename)
+        return mime_type
+
     try:
+        media_type = get_mime_type(filename)
         file_obj = s3_client.get_object(Bucket="memes", Key=filename)
-        return StreamingResponse(file_obj['Body'], media_type='application/octet-stream')
+        return StreamingResponse(file_obj['Body'], media_type=media_type)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.get("/files")
+async def get_list_files():
+    try:
+        response = s3_client.list_objects_v2(Bucket="memes")
+        files = [item["Key"] for item in response.get("Contents", [])]
+        return {"files": files}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.delete("/delete/{filename}")
+async def delete_file(filename: str):
+    try:
+        s3_client.delete_object(Bucket="memes", Key=filename)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        return {"error": str(e)}
