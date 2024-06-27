13cab653fec5ab855753da07d5e2ad7566bf448c
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-06-27 18:50:23 +0300

    added endpoint for adding memes to public service

--- public_memes_api/memes/router.py
+++ public_memes_api/memes/router.py
@@ -1,4 +1,13 @@
+from pathlib import Path
+from typing import Optional
+
+import httpx
 from fastapi import APIRouter, HTTPException, UploadFile, File, Response, status
+
+from public_memes_api.memes.dao import MemesDAO
+from public_memes_api.config.config import PRIVATE_MEDIA_SERVICE_URL
+from public_memes_api.memes.exceptions import AddingMemePictureException, AddingMemeMetadataException
+from public_memes_api.memes.schemas import SMemeCreate
 
 router = APIRouter(
     prefix="/memes",
@@ -6,58 +15,36 @@ router = APIRouter(
 )
 
 
-@router.get("/memes")
-def read_memes(skip: int = 0, limit: int = 10):
-    memes = get_memes(db, skip=skip, limit=limit)
-    return memes
+# @router.get("/memes")
+# async def get_memes(skip: int = 0, limit: int = 10):
+#     memes = await MemesDAO.get_memes_with_pagination(skip, limit)
+#     return memes
 
 
-@router.get("/memes/{meme_id}")
-def read_meme(meme_id: int, db: Session = Depends(get_db)):
-    meme = get_meme(db, meme_id=meme_id)
-    if meme is None:
-        raise HTTPException(status_code=404, detail="Meme not found")
-    return meme
+# @router.get("/memes/{meme_id}")
+# async def get_meme(meme_id: int):
+#     meme = MemesDAO.find_by_id(meme_id)
+#     if meme is None:
+#         raise HTTPException(status_code=404, detail="Meme not found")
+#     return meme
 
 
 @router.post("/memes")
-async def create_meme(meme: MemeCreate, file: UploadFile = File(...), db: Session = Depends(get_db)):
-    async with httpx.AsyncClient() as client:
-        # Загрузка файла на приватный сервис
+async def create_meme(meme_name: str, text: Optional[str], file: UploadFile = File(...)) -> int:  # meme: SMemeCreate
+    id_added_meme: int = await MemesDAO.add(file_name=meme_name, text=text)
+
+    if not isinstance(id_added_meme, int):
+        raise AddingMemeMetadataException
+
+    file_extension = Path(file.filename).suffix[1:]
+    new_name = f"{meme_name}_{id_added_meme}.{file_extension}"
+
+    async with httpx.AsyncClient() as client:  # Загрузка файла на приватный сервис
         response = await client.post(
-            f"{PRIVATE_MEDIA_SERVICE_URL}/upload",
-            files={"file": (file.filename, file.file, file.content_type)}
+            f"{PRIVATE_MEDIA_SERVICE_URL}/s3_memes/upload",
+            files={"file": (new_name, file.file, file.content_type)}
         )
-        if response.status_code != 200:
-            raise HTTPException(status_code=response.status_code, detail="Failed to upload image to media service")
+        if response.status_code != 201:
+            raise AddingMemePictureException
 
-        file_url = response.json().get("filename")
-
-    # Сохранение данных мема в базе данных
-    meme.image_url = file_url
-    return create_meme(db, meme=meme)
-
-
-@router.put("/memes/{meme_id}")
-async def update_meme(meme_id: int, meme: MemeUpdate, db: Session = Depends(get_db)):
-    db_meme = update_meme(db, meme_id=meme_id, meme=meme)
-    if db_meme is None:
-        raise HTTPException(status_code=404, detail="Meme not found")
-    return db_meme
-
-
-@router.delete("/memes/{meme_id}")
-async def delete_meme(meme_id: int, db: Session = Depends(get_db)):
-    # Получение информации о меме перед удалением
-    meme = get_meme(db, meme_id=meme_id)
-    if meme is None:
-        raise HTTPException(status_code=404, detail="Meme not found")
-
-    async with httpx.AsyncClient() as client:
-        # Удаление файла на приватном сервисе
-        response = await client.delete(f"{PRIVATE_MEDIA_SERVICE_URL}/delete/{meme.image_url}")
-        if response.status_code != 200:
-            raise HTTPException(status_code=response.status_code, detail="Failed to delete image from media service")
-
-    delete_meme(db, meme_id=meme_id)
-    return {"message": "Meme deleted successfully"}
+    return id_added_meme
