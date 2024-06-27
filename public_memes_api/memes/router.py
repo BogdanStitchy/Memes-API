6d9e60a494eafc97e9abaa634e69c36928739a64
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-06-27 19:47:52 +0300

    added endpoint to public service to receive memes

--- public_memes_api/memes/router.py
+++ public_memes_api/memes/router.py
@@ -1,12 +1,15 @@
+from io import BytesIO
 from pathlib import Path
 from typing import Optional
 
 import httpx
 from fastapi import APIRouter, HTTPException, UploadFile, File, Response, status
+from starlette.responses import StreamingResponse
 
 from public_memes_api.memes.dao import MemesDAO
 from public_memes_api.config.config import PRIVATE_MEDIA_SERVICE_URL
-from public_memes_api.memes.exceptions import AddingMemePictureException, AddingMemeMetadataException
+from public_memes_api.memes.exceptions import AddingMemePictureException, AddingMemeMetadataException, \
+    IncorrectMemeIdException, MemeImageException
 from public_memes_api.memes.schemas import SMemeCreate
 
 router = APIRouter(
@@ -21,23 +24,35 @@ router = APIRouter(
 #     return memes
 
 
-# @router.get("/memes/{meme_id}")
-# async def get_meme(meme_id: int):
-#     meme = MemesDAO.find_by_id(meme_id)
-#     if meme is None:
-#         raise HTTPException(status_code=404, detail="Meme not found")
-#     return meme
+@router.get("/{meme_id}")
+async def get_meme(meme_id: int) -> StreamingResponse:
+    meme = await MemesDAO.find_by_id(meme_id)
+    print(f"{meme=}")
+    print(f"{type(meme)=}")
+    if meme is None:
+        raise IncorrectMemeIdException
+
+    image_name = f"{meme.id}_{meme.file_name}"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"{PRIVATE_MEDIA_SERVICE_URL}/s3_memes/download/{image_name}")
+
+        if response.status_code != 200:
+            raise MemeImageException
+
+    content_type = response.headers.get("content-type", "application/octet-stream")
+
+    return StreamingResponse(BytesIO(response.content), media_type=content_type)
 
 
-@router.post("/memes")
-async def create_meme(meme_name: str, text: Optional[str], file: UploadFile = File(...)) -> int:  # meme: SMemeCreate
-    id_added_meme: int = await MemesDAO.add(file_name=meme_name, text=text)
+@router.post("/meme")
+async def create_meme(text: Optional[str], file: UploadFile = File(...)) -> int:
+    id_added_meme: int = await MemesDAO.add(file_name=file.filename, text=text)
 
     if not isinstance(id_added_meme, int):
         raise AddingMemeMetadataException
 
-    file_extension = Path(file.filename).suffix[1:]
-    new_name = f"{meme_name}_{id_added_meme}.{file_extension}"
+    new_name = f"{id_added_meme}_{file.filename}"
 
     async with httpx.AsyncClient() as client:  # Загрузка файла на приватный сервис
         response = await client.post(
