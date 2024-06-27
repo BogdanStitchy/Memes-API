b526c1882a84bfdab285196e2e28d9aedcd59a9e
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-06-27 23:25:16 +0300

    added a public service endpoint to update memes

--- public_memes_api/memes/router.py
+++ public_memes_api/memes/router.py
@@ -10,6 +10,7 @@ from public_memes_api.config.config import PRIVATE_MEDIA_SERVICE_URL
 from public_memes_api.memes.exceptions import AddingMemePictureException, AddingMemeMetadataException, \
     IncorrectMemeIdException, MemeImageException, MemeMetadataException
 from public_memes_api.memes.schemas import SMemeRead, SAddedId
+from public_memes_api.memes.utils_s3 import upload_image_to_s3, delete_image_from_s3
 
 router = APIRouter(
     prefix="/memes",
@@ -68,12 +69,47 @@ async def add_meme(text: Optional[str], file: UploadFile = File(...)) -> SAddedI
 
     new_name = f"{id_added_meme}_{file.filename}"
 
-    async with httpx.AsyncClient() as client:  # Загрузка файла на приватный сервис
-        response = await client.post(
-            f"{PRIVATE_MEDIA_SERVICE_URL}/s3_memes/upload",
-            files={"file": (new_name, file.file, file.content_type)}
-        )
-        if response.status_code != 201:
-            raise AddingMemePictureException
+    # async with httpx.AsyncClient() as client:  # Загрузка файла на приватный сервис
+    #     response = await client.post(
+    #         f"{PRIVATE_MEDIA_SERVICE_URL}/s3_memes/upload",
+    #         files={"file": (new_name, file.file, file.content_type)}
+    #     )
+    #     if response.status_code != 201:
+    #         raise AddingMemePictureException
+    await upload_image_to_s3(new_name, file)
 
     return SAddedId(id_added_meme=id_added_meme)
+
+
+@router.put("/{meme_id}")
+async def update_meme(
+        background_tasks: BackgroundTasks,
+        meme_id: int,
+        text: Optional[str] = None,
+        file: UploadFile = File(None)
+):
+    meme = await MemesDAO.find_by_id(meme_id)
+
+    if meme is None:
+        raise IncorrectMemeIdException
+    if isinstance(meme, dict):
+        if "error" in meme:
+            raise MemeMetadataException
+
+    if file:
+        old_image_name = f"{meme.id}_{meme.file_name}"
+        new_image_name = f"{meme.id}_{file.filename}"
+
+        # чтобы не удалить новый мем, если имена обновленного и старого мема совпадают
+        await upload_image_to_s3(new_image_name, file)
+        if old_image_name != new_image_name:
+            background_tasks.add_task(delete_image_from_s3, old_image_name)  # Удаляем старое изображение в фоне
+
+        meme.file_name = file.filename
+
+    if text is not None:
+        meme.text = text
+
+    await MemesDAO.update(meme_id, file_name=meme.file_name, text=meme.text)
+
+    return Response(status_code=status.HTTP_200_OK)
