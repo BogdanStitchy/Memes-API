4d3e3a6f7a271829586e35b23b9d6393316cb2a8
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-06-28 23:52:40 +0300

    deleted unnecessary comments

--- public_memes_api/memes/utils_s3.py
+++ public_memes_api/memes/utils_s3.py
@@ -6,10 +6,8 @@ from public_memes_api.memes.exceptions import MemeImageDeleteException, AddingMe
 
 
 async def delete_image_from_s3(image_name: str) -> None:
-    print("Task Start")
     async with httpx.AsyncClient() as client:
         response = await client.delete(f"{PRIVATE_MEDIA_SERVICE_URL}/s3_memes/delete/{image_name}")
-        print("Task Finish", response.status_code)
         if response.status_code != 204:
             raise MemeImageDeleteException
 
