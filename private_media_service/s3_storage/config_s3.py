4559a57d9c7e97500f4707e72d9900ac8b862c09
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-07-02 19:13:42 +0300

    added a function to create a bucket in storage, if there is no bucket

--- private_media_service/s3_storage/config_s3.py
+++ private_media_service/s3_storage/config_s3.py
@@ -1,6 +1,15 @@
 import boto3
+from botocore.exceptions import ClientError
 from moto import mock_aws
 from private_media_service.config.config import MODE, LOGIN_S3, PASSWORD_S3, HOST_S3
+
+
+def create_bucket_if_not_exists(s3_client, bucket_name):
+    try:
+        s3_client.head_bucket(Bucket=bucket_name)  # проверка существования бакета
+    except ClientError:
+        s3_client.create_bucket(Bucket=bucket_name)  # бакет не существует, создаем новый бакет
+
 
 if MODE == "TEST":
     mock = mock_aws()
@@ -15,3 +24,4 @@ else:
         aws_secret_access_key=PASSWORD_S3,
         region_name='us-east-1'
     )
+    create_bucket_if_not_exists(s3_client, "memes")
