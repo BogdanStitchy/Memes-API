4cce8be625b80722526070b2cf5d2503f3948e3d
Progger715 <83240866+Progger715@users.noreply.github.com>
2024-07-02 18:52:53 +0300

    added CORS setting for private service

--- private_media_service/main.py
+++ private_media_service/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from private_media_service.memes.router import router
 
 app = FastAPI()
 
 app.include_router(router)
 
+origins = [
+    "http://localhost:8000",
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
+    "http://127.0.0.1:8001",
+]
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Разрешенные источники
+    allow_credentials=True,  # Разрешение на отправку cookie
+    allow_methods=["GET", "POST", "DELETE"],  # Разрешенные методы
+)
