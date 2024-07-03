ee9e1539b919b46b7fadf06f3078e45864cfdeba
Bogdan <83240866+BogdanStitchy@users.noreply.github.com>
2024-07-03 16:10:23 +0300

    unused comments removed

--- public_memes_api/tests/conftest.py
+++ public_memes_api/tests/conftest.py
@@ -20,33 +20,7 @@ async def prepare_database():
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
-    print("Database created sdfsdf")
-
-    # def open_mock_json(model: str):
-    #     with open(f"app/tests/mock_{model}.json", encoding="utf-8") as file:
-    #         return json.load(file)
-    #
-    # hotels = open_mock_json("hotels")
-    # rooms = open_mock_json("rooms")
-    # users = open_mock_json("users")
-    # bookings = open_mock_json("bookings")
-    #
-    # for booking in bookings:
-    #     booking["date_from"] = datetime.strptime(booking["date_from"], "%Y-%m-%d")
-    #     booking["date_to"] = datetime.strptime(booking["date_to"], "%Y-%m-%d")
-    #
-    # async with async_session_maker() as session:
-    #     add_hotels = insert(Hotels).values(hotels)
-    #     add_rooms = insert(Rooms).values(rooms)
-    #     add_users = insert(Users).values(users)
-    #     add_bookings = insert(Bookings).values(bookings)
-    #
-    #     await session.execute(add_hotels)
-    #     await session.execute(add_rooms)
-    #     await session.execute(add_users)
-    #     await session.execute(add_bookings)
-    #
-    #     await session.commit()
+    print("Database created")
 
 
 @pytest.fixture(scope="function")
